33c09fc5040de5e33e35f45a9e2293fc2b2d8f40
Hao Shi <shisoftgenius@gmail.com>
2016-12-14 14:27:56 +0800

    completely mut free client

--- src/raft/client.rs
+++ src/raft/client.rs
@@ -2,7 +2,7 @@ use raft::{SyncClient, ClientClusterInfo, RaftMsg, RaftStateMachine, LogEntry, C
 use raft::state_machine::OpType;
 use std::collections::{HashMap, BTreeMap, HashSet};
 use std::iter::FromIterator;
-use std::sync::Mutex;
+use std::sync::{Mutex, RwLock};
 use std::sync::atomic::{AtomicU64, Ordering};
 use std::cell::RefCell;
 use bifrost_plugins::hash_str;
@@ -11,8 +11,6 @@ use rand;
 const ORDERING: Ordering = Ordering::Relaxed;
 
 struct QryMeta {
-    last_log_id: AtomicU64,
-    last_log_term: AtomicU64,
     pos: AtomicU64
 }
 
@@ -23,23 +21,25 @@ struct Members {
 
 pub struct RaftClient {
     qry_meta: QryMeta,
-    members: Members,
-    term: u64, // last member list update term
+    members: RwLock<Members>,
+    leader_id: AtomicU64,
+    last_log_id: AtomicU64,
+    last_log_term: AtomicU64,
 }
 
 impl RaftClient {
     pub fn new(servers: Vec<String>) -> Option<RaftClient> {
         let mut client = RaftClient {
             qry_meta: QryMeta {
-                last_log_id: AtomicU64::new(0),
-                last_log_term: AtomicU64::new(0),
                 pos: AtomicU64::new(rand::random::<u64>())
             },
-            members: Members {
+            members: RwLock::new(Members {
                 clients: BTreeMap::new(),
                 id_map: HashMap::new()
-            },
-            term: 0,
+            }),
+            leader_id: AtomicU64::new(0),
+            last_log_id: AtomicU64::new(0),
+            last_log_term: AtomicU64::new(0),
         };
         match client.update_info(&HashSet::from_iter(servers)) {
             Ok(_) => Some(client),
@@ -47,12 +47,13 @@ impl RaftClient {
         }
     }
 
-    fn update_info(&mut self, addrs: &HashSet<String>) -> Result<(), ()> {
+    fn update_info(&self, addrs: &HashSet<String>) -> Result<(), ()> {
         let info: ClientClusterInfo;
         let mut servers = None;
+        let mut members = self.members.write().unwrap();
         for server_addr in addrs {
             let id = hash_str(server_addr.clone());
-            let mut client = self.members.clients.entry(id).or_insert_with(|| {
+            let mut client = members.clients.entry(id).or_insert_with(|| {
                 Mutex::new(SyncClient::new(server_addr))
             });
             if let Some(Ok(info)) = client.lock().unwrap().c_server_cluster_info() {
@@ -64,19 +65,19 @@ impl RaftClient {
             Some(servers) => {
                 let remote_members = servers.members;
                 let mut remote_ids = HashSet::with_capacity(remote_members.len());
-                self.members.id_map.clear();
+                members.id_map.clear();
                 for (id, addr) in remote_members {
-                    self.members.id_map.insert(id, addr);
+                    members.id_map.insert(id, addr);
                     remote_ids.insert(id);
                 }
-                let mut connected_ids = HashSet::with_capacity(self.members.clients.len());
-                for id in self.members.clients.keys() {connected_ids.insert(*id);}
+                let mut connected_ids = HashSet::with_capacity(members.clients.len());
+                for id in members.clients.keys() {connected_ids.insert(*id);}
                 let ids_to_remove = connected_ids.difference(&remote_ids);
-                for id in ids_to_remove {self.members.clients.remove(id);}
+                for id in ids_to_remove {members.clients.remove(id);}
                 for id in remote_ids.difference(&connected_ids) {
-                    let addr = self.members.id_map.get(id).unwrap();
-                    self.members.clients.entry(id.clone()).or_insert_with(|| {
-                        Mutex::new(SyncClient::new(addr))
+                    let addr = members.id_map.get(id).unwrap().clone();
+                    members.clients.entry(id.clone()).or_insert_with(|| {
+                        Mutex::new(SyncClient::new(&addr))
                     });
                 }
                 Ok(())
@@ -101,14 +102,17 @@ impl RaftClient {
 
     fn query(&self, sm_id: u64, fn_id: u64, data: &Vec<u8>) -> Option<Vec<u8>> {
         let pos = self.qry_meta.pos.fetch_add(1, ORDERING);
-        let clients_count = self.members.clients.len();
         let res = {
-            let mut client = self.members.clients.values()
-                .nth(pos as usize % clients_count)
-                .unwrap().lock().unwrap();
+            let members = self.members.read().unwrap();
+            let mut client = {
+                let clients_count = members.clients.len();
+                members.clients.values()
+                    .nth(pos as usize % clients_count)
+                    .unwrap().lock().unwrap()
+            };
             client.c_query(LogEntry {
-                id: self.qry_meta.last_log_id.load(ORDERING),
-                term: self.qry_meta.last_log_term.load(ORDERING),
+                id: self.last_log_id.load(ORDERING),
+                term: self.last_log_term.load(ORDERING),
                 sm_id: sm_id,
                 fn_id: fn_id,
                 data: data.clone()
@@ -125,8 +129,8 @@ impl RaftClient {
                         last_log_term: last_log_term,
                         last_log_id: last_log_id
                     } => {
-                        swap_when_larger(&self.qry_meta.last_log_id, last_log_id);
-                        swap_when_larger(&self.qry_meta.last_log_term, last_log_term);
+                        swap_when_larger(&self.last_log_id, last_log_id);
+                        swap_when_larger(&self.last_log_term, last_log_term);
                         Some(data)
                     },
                 }
@@ -134,6 +138,10 @@ impl RaftClient {
             _ => None
         }
     }
+
+//    fn command(&self, sm_id: u64, fn_id: u64, data: &Vec<u8>) -> Option<u8> {
+//
+//    }
 }
 
 fn swap_when_larger(atomic: &AtomicU64, value: u64) {
