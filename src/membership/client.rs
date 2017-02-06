1c6895c775cf344dec7c7ef7cbdd044e71e60b52
Hao Shi <shisoftgenius@gmail.com>
2017-02-06 13:26:44 +0800

    membership client subscriptions

--- src/membership/client.rs
+++ src/membership/client.rs
@@ -1,5 +1,5 @@
 use std::sync::Arc;
-use raft::client::RaftClient;
+use raft::client::{RaftClient, SubscriptionError};
 use raft::state_machine::master::ExecError;
 use bifrost_hasher::hash_str;
 use super::raft::client::SMClient;
@@ -63,5 +63,49 @@ impl Client {
     pub fn all_members(&self, online_only: bool) -> Result<Result<Vec<Member>, ()>, ExecError> {
         self.sm_client.all_members(online_only)
     }
-    // TODO: Subscription functions
+    pub fn on_group_member_offline<F>(&self, f: F, group: String)
+        -> Result<Result<u64, SubscriptionError>, ExecError>
+        where F: Fn(Result<Member, ()>)  + 'static + Send + Sync {
+        self.sm_client.on_group_member_offline(f, hash_str(group))
+    }
+    pub fn on_any_member_offline<F>(&self, f: F)
+        -> Result<Result<u64, SubscriptionError>, ExecError>
+        where F: Fn(Result<Member, ()>)  + 'static + Send + Sync {
+        self.sm_client.on_any_member_offline(f)
+    }
+    pub fn on_group_member_online<F>(&self, f: F, group: String)
+        -> Result<Result<u64, SubscriptionError>, ExecError>
+        where F: Fn(Result<Member, ()>)  + 'static + Send + Sync {
+        self.sm_client.on_group_member_online(f, hash_str(group))
+    }
+    pub fn on_any_member_online<F>(&self, f: F)
+        -> Result<Result<u64, SubscriptionError>, ExecError>
+        where F: Fn(Result<Member, ()>)  + 'static + Send + Sync {
+        self.sm_client.on_any_member_online(f)
+    }
+    pub fn on_group_member_joined<F>(&self, f: F, group: String)
+        -> Result<Result<u64, SubscriptionError>, ExecError>
+        where F: Fn(Result<Member, ()>)  + 'static + Send + Sync {
+        self.sm_client.on_group_member_joined(f, hash_str(group))
+    }
+    pub fn on_any_member_joined<F>(&self, f: F)
+        -> Result<Result<u64, SubscriptionError>, ExecError>
+        where F: Fn(Result<Member, ()>)  + 'static + Send + Sync {
+        self.sm_client.on_any_member_joined(f)
+    }
+    pub fn on_group_member_left<F>(&self, f: F, group: String)
+        -> Result<Result<u64, SubscriptionError>, ExecError>
+        where F: Fn(Result<Member, ()>)  + 'static + Send + Sync {
+        self.sm_client.on_group_member_left(f, hash_str(group))
+    }
+    pub fn on_any_member_left<F>(&self, f: F)
+        -> Result<Result<u64, SubscriptionError>, ExecError>
+        where F: Fn(Result<Member, ()>)  + 'static + Send + Sync {
+        self.sm_client.on_any_member_left(f)
+    }
+    pub fn on_group_leader_changed<F>(&self, f: F, group: String)
+        -> Result<Result<u64, SubscriptionError>, ExecError>
+        where F: Fn(Result<(Member, Member), ()>)  + 'static + Send + Sync {
+        self.sm_client.on_group_leader_changed(f, hash_str(group))
+    }
 }
