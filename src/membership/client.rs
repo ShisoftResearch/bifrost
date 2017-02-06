abf034c4823982f4c88f36ca3c67e192fac3dfc4
Hao Shi <shisoftgenius@gmail.com>
2017-02-06 09:07:01 +0800

    bridge functions

--- src/membership/client.rs
+++ src/membership/client.rs
@@ -1,3 +1,7 @@
+use std::sync::Arc;
+use bifrost_hasher::hash_str;
+use super::raft::client::SMClient;
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Member {
     pub id: u64,
@@ -12,3 +16,24 @@ pub struct Group {
     pub members: u64,
 }
 
+pub struct MemberClient {
+    pub id: u64,
+    pub sm_client: Arc<SMClient>
+}
+
+impl MemberClient {
+    pub fn join_group(&self, group: String) -> Option<Result<(), ()>> {
+        if let Ok(r) = self.sm_client.join_group(hash_str(group), self.id) {
+            Some(r)
+        } else {
+            None
+        }
+    }
+    pub fn leave_group(&self, group: String) -> Option<Result<(), ()>> {
+        if let Ok(r) = self.sm_client.leave_group(hash_str(group), self.id) {
+            Some(r)
+        } else {
+            None
+        }
+    }
+}
--- src/membership/member.rs
+++ src/membership/member.rs
@@ -2,6 +2,7 @@ use raft::client::RaftClient;
 use super::DEFAULT_SERVICE_ID;
 use super::heartbeat_rpc::*;
 use super::raft::client::SMClient;
+use super::client::MemberClient;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::{thread, time};
@@ -10,7 +11,8 @@ use bifrost_hasher::hash_str;
 static PING_INTERVAL: u64 = 500; //5 secs for 500ms heartbeat
 
 pub struct MemberService {
-    pub sm_client: Arc<SMClient>,
+    member_client: MemberClient,
+    sm_client: Arc<SMClient>,
     raft_client: Arc<RaftClient>,
     address: String,
     closed: AtomicBool,
@@ -19,13 +21,18 @@ pub struct MemberService {
 
 impl MemberService {
     pub fn new(server_address: String, raft_client: &Arc<RaftClient>) -> Arc<MemberService> {
+        let server_id = hash_str(server_address.clone());
         let sm_client = Arc::new(SMClient::new(DEFAULT_SERVICE_ID, &raft_client));
         let service = Arc::new(MemberService {
             sm_client: sm_client.clone(),
-            raft_client:  raft_client.clone(),
+            member_client: MemberClient {
+                id: server_id,
+                sm_client: sm_client.clone()
+            },
+            raft_client: raft_client.clone(),
             address: server_address.clone(),
             closed: AtomicBool::new(false),
-            id: hash_str(server_address.clone()),
+            id: server_id,
         });
         sm_client.join(server_address);
         let service_clone = service.clone();
@@ -47,6 +54,12 @@ impl MemberService {
     pub fn leave(&self) {
         self.sm_client.leave(self.id);
         self.close();
+    }
+    pub fn join_group(&self, group: String) -> Option<Result<(), ()>> {
+        self.member_client.join_group(group)
+    }
+    pub fn leave_group(&self, group: String) -> Option<Result<(), ()>> {
+        self.member_client.leave_group(group)
     }
 }
 
