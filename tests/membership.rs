80bd1d2a620c967772f6ef3a8345c5bfedf611c8
Hao Shi <shisoftgenius@gmail.com>
2017-02-13 05:51:38 +0800

    membership changes callback check

--- tests/membership.rs
+++ tests/membership.rs
@@ -6,7 +6,8 @@ use bifrost::membership::client::Client;
 use bifrost::raft::client::RaftClient;
 use bifrost::raft::state_machine::callback::client::SubscriptionService;
 
-use std::mem::forget;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 use raft::wait;
 
@@ -38,39 +39,68 @@ fn primary() {
     client.new_group(&group_2).unwrap().unwrap();
     client.new_group(&group_3).unwrap().unwrap();
 
-    client.on_any_member_joined(|res| {
+    let any_member_joined_count = Arc::new(AtomicUsize::new(0));
+    let any_member_left_count = Arc::new(AtomicUsize::new(0));
+    let any_member_offline_count = Arc::new(AtomicUsize::new(0));
+    let any_member_online_count = Arc::new(AtomicUsize::new(0));
+    let group_leader_changed_count = Arc::new(AtomicUsize::new(0));
+    let group_member_joined_count = Arc::new(AtomicUsize::new(0));
+    let group_member_left_count = Arc::new(AtomicUsize::new(0));
+    let group_member_online_count = Arc::new(AtomicUsize::new(0));
+    let group_member_offline_count = Arc::new(AtomicUsize::new(0));
+
+    let any_member_joined_count_clone = any_member_joined_count.clone();
+    let any_member_left_count_clone = any_member_left_count.clone();
+    let any_member_offline_count_clone = any_member_offline_count.clone();
+    let any_member_online_count_clone = any_member_online_count.clone();
+    let group_leader_changed_count_clone = group_leader_changed_count.clone();
+    let group_member_joined_count_clone = group_member_joined_count.clone();
+    let group_member_left_count_clone = group_member_left_count.clone();
+    let group_member_online_count_clone = group_member_online_count.clone();
+    let group_member_offline_count_clone = group_member_offline_count.clone();
+
+    client.on_any_member_joined(move |res| {
+        any_member_joined_count_clone.fetch_add(1, Ordering::Relaxed);
         println!("NEW MEMBER: {:?}", res.unwrap().address);
     }).unwrap().unwrap();
 
-    client.on_any_member_left(|res| {
+    client.on_any_member_left(move |res| {
+        any_member_left_count_clone.fetch_add(1, Ordering::Relaxed);
         println!("MEMBER LEFT: {:?}", res.unwrap().address);
     }).unwrap().unwrap();
 
-    client.on_any_member_offline(|res| {
+    client.on_any_member_offline(move |res| {
+        any_member_offline_count_clone.fetch_add(1, Ordering::Relaxed);
         println!("MEMBER OFFLINE: {:?}", res.unwrap().address);
     }).unwrap().unwrap();
 
-    client.on_any_member_online(|res| {
+    client.on_any_member_online(move |res| {
+        any_member_online_count_clone.fetch_add(1, Ordering::Relaxed);
         println!("MEMBER ONLINE: {:?}", res.unwrap().address);
     }).unwrap().unwrap();
 
-    client.on_group_leader_changed(|res| {
+    client.on_group_leader_changed(move |res| {
+        group_leader_changed_count_clone.fetch_add(1, Ordering::Relaxed);
         println!("GROUP LEADER CHANGED");
     }, &group_1).unwrap().unwrap();
 
-    client.on_group_member_joined(|res| {
+    client.on_group_member_joined(move |res| {
+        group_member_joined_count_clone.fetch_add(1, Ordering::Relaxed);
         println!("GROUP MEMBER JOINED: {:?}", res.unwrap().address);
     }, &group_1).unwrap().unwrap();
 
-    client.on_group_member_left(|res| {
+    client.on_group_member_left(move |res| {
+        group_member_left_count_clone.fetch_add(1, Ordering::Relaxed);
         println!("GROUP MEMBER LEFT: {:?}", res.unwrap().address);
     }, &group_1).unwrap().unwrap();
 
-    client.on_group_member_online(|res| {
+    client.on_group_member_online(move |res| {
+        group_member_online_count_clone.fetch_add(1, Ordering::Relaxed);
         println!("GROUP MEMBER ONLINE: {:?}", res.unwrap().address);
     }, &group_1).unwrap().unwrap();
 
-    client.on_group_member_offline(|res| {
+    client.on_group_member_offline(move |res| {
+        group_member_offline_count_clone.fetch_add(1, Ordering::Relaxed);
         println!("GROUP MEMBER OFFLINE: {:?}", res.unwrap().address);
     }, &group_1).unwrap().unwrap();
 
@@ -136,4 +166,14 @@ fn primary() {
     assert_eq!(member1_svr.client().group_members(&group_3, true).unwrap().unwrap().len(), 0);
 
     wait();
+
+    assert_eq!(any_member_joined_count.load(Ordering::Relaxed), 3);
+    assert_eq!(any_member_left_count.load(Ordering::Relaxed), 1);
+    assert_eq!(any_member_offline_count.load(Ordering::Relaxed), 1);
+    assert_eq!(any_member_online_count.load(Ordering::Relaxed), 0); // no server online from offline
+    assert_eq!(group_leader_changed_count.load(Ordering::Relaxed), 3); // 1 for None -> server1, 2 for server 1 -> server 2, 3 for server 2 -> server 3
+    assert_eq!(group_member_joined_count.load(Ordering::Relaxed), 3);
+    assert_eq!(group_member_left_count.load(Ordering::Relaxed), 1);
+    assert_eq!(group_member_online_count.load(Ordering::Relaxed), 0);
+    assert_eq!(group_member_offline_count.load(Ordering::Relaxed), 1);
 }
