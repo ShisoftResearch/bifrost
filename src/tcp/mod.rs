a3d0a2562596de0cfd67f3840a4a6254823ec55e
Jack Shi <shisoftgenius@gmail.com>
2017-04-28 01:37:48 +0800

    default runtime string for standalone address

--- src/tcp/mod.rs
+++ src/tcp/mod.rs
@@ -5,3 +5,7 @@ pub mod client;
 pub mod shortcut;
 
 pub static STANDALONE_ADDRESS: &'static str = "STANDALONE";
+
+lazy_static! {
+    pub static ref STANDALONE_ADDRESS_STRING: String = String::from(STANDALONE_ADDRESS);
+}
