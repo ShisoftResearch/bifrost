698ab0c08663daf4dd2f10a009a487008794760e
Jack Shi <shisoftgenius@gmail.com>
2017-06-18 21:27:24 +0800

    twox hash as secondary hasher

--- src/hasher/src/lib.rs
+++ src/hasher/src/lib.rs
@@ -1,6 +1,8 @@
 use std::collections::hash_map::DefaultHasher;
 use std::hash::Hasher;
 
+extern crate twox_hash;
+
 pub fn hash_bytes(bytes: &[u8]) -> u64 {
     let mut hasher = DefaultHasher::default();
     hasher.write(bytes);
@@ -10,4 +12,10 @@ pub fn hash_bytes(bytes: &[u8]) -> u64 {
 pub fn hash_str (text: &String) -> u64 { // the same as the one in utils hash
     let text_bytes = text.as_bytes();
     hash_bytes(text_bytes)
+}
+
+pub fn hash_bytes_secondary(bytes: &[u8]) -> u64 {
+    let mut hasher = twox_hash::XxHash::default();
+    hasher.write(bytes);
+    hasher.finish()
 }
