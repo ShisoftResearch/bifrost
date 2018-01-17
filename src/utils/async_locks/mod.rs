1b624667313b19fda90150f271c42f8814f5bd36
Hao Shi <shisoftgenius@gmail.com>
2018-01-18 02:47:28 +0800

    export mutex guard

--- src/utils/async_locks/mod.rs
+++ src/utils/async_locks/mod.rs
@@ -1,5 +1,5 @@
 pub mod mutex;
 pub mod rwlock;
 
-pub use self::mutex::{Mutex, AsyncMutexGuard};
+pub use self::mutex::{Mutex, AsyncMutexGuard, MutexGuard};
 pub use self::rwlock::{RwLock, AsyncRwLockWriteGuard, AsyncRwLockReadGuard};
