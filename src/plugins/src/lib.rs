b60bb2d7aceace32d4b7290ec4d8874cd54f85c7
Hao Shi <shisoftgenius@gmail.com>
2023-10-30 16:59:58 +0000

    supporting string literal

--- src/plugins/src/lib.rs
+++ src/plugins/src/lib.rs
@@ -1,12 +1,15 @@
 extern crate proc_macro;
 extern crate bifrost_hasher;
+extern crate syn;
 
 use proc_macro::TokenStream;
 use bifrost_hasher::hash_str;
 use proc_macro::TokenTree;
+use syn::{parse_macro_input, LitStr};
 
 #[proc_macro]
 pub fn hash_ident(item: TokenStream) -> TokenStream {
+    let item_clone = item.clone();
     let tokens: Vec<_> = item.into_iter().collect();
     if tokens.len() != 1 {
         panic!("argument should be a single identifier, but got {} arguments {:?}",
@@ -14,6 +17,7 @@ pub fn hash_ident(item: TokenStream) -> TokenStream {
     }
     let text = match tokens[0] {
         TokenTree::Ident(ref ident) => ident.to_string(),
+        TokenTree::Literal(_) => parse_macro_input!(item_clone as LitStr).value(),
         _ => tokens[0].to_string().trim().to_owned()
     };
     let text = &*text;
