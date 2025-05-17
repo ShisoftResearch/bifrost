2b8baf5f2f1798485b0eabe6af0fe7d15ad54755
Hao Shi <shisoftgenius@gmail.com>
2025-05-17 21:31:03 +0000

    provide all server ids

--- src/rpc/cluster.rs
+++ src/rpc/cluster.rs
@@ -1,6 +1,6 @@
 use std::{future::Future, sync::Arc};
 
-use crate::{conshash::ConsistentHashing, rpc::{RPCError, DEFAULT_CLIENT_POOL}};
+use crate::{conshash::ConsistentHashing, raft::state_machine::master::ExecError, rpc::{RPCError, DEFAULT_CLIENT_POOL}};
 use futures::stream::FuturesUnordered;
 use tokio_stream::StreamExt;
 
@@ -9,29 +9,29 @@ use super::{RPCClient, ServiceClientWithId};
 pub async fn broadcast_to_members<C, F, R, Fut>(
     conshash: &Arc<ConsistentHashing>,
     func: F,
-) -> Result<Vec<(u64, Result<R, RPCError>)>, String>
+) -> Result<Vec<(u64, Result<R, RPCError>)>, ExecError>
 where
     C: ServiceClientWithId,
     F: Fn(Arc<C>) -> Fut + Clone + Send + 'static,
     Fut: Future<Output = Result<R, RPCError>> + Send,
 {
-    let (members, _) = match conshash.membership().all_members(true).await {
-        Ok(members) => members,
-        Err(e) => {
-            return Err(format!("Failed to get all members: {:?}", e));
-        }
-    };
-    let server_ids = members.iter().map(|m| &m.id);
+    let server_ids = all_server_ids(&conshash).await?;
     broadcast_with_server_ids(server_ids, &conshash, func).await
 }
 
-pub async fn broadcast_with_server_ids<'a, C, F, R, I, Fut>(
+pub async fn all_server_ids(conshash: &Arc<ConsistentHashing>) -> Result<impl Iterator<Item = u64>, ExecError> {
+    let (members, _) = conshash.membership().all_members(true).await?;
+    Ok(members.into_iter().map(|m| m.id))
+}
+
+
+pub async fn broadcast_with_server_ids<C, F, R, I, Fut>(
     server_ids: I,
     conshash: &Arc<ConsistentHashing>,
     func: F,
-) -> Result<Vec<(u64, Result<R, RPCError>)>, String>
+) -> Result<Vec<(u64, Result<R, RPCError>)>, ExecError>
 where
-    I: Iterator<Item = &'a u64>,
+    I: Iterator<Item = u64>,
     C: ServiceClientWithId,
     F: Fn(Arc<C>) -> Fut + Clone + Send + 'static,
     Fut: Future<Output = Result<R, RPCError>> + Send,
@@ -40,14 +40,14 @@ where
         .map(|sid| {
             let func = func.clone();
             async move {
-                let client = match client_by_server_id(&conshash, *sid).await {
+                let client = match client_by_server_id(&conshash, sid).await {
                     Ok(client) => client,
                     Err(e) => {
                         error!("Failed to get client by server id {}: {:?}", sid, e);
-                        return (*sid, Err(e));
+                        return (sid, Err(e));
                     }
                 };
-                return (*sid, func(client).await);
+                return (sid, func(client).await);
             }
         })
         .collect();
